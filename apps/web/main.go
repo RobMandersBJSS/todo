907c7d724fa1ce50c1c0203a752b124d2ba28a34
Robert Manders <robert.manders@bjss.com>
2024-05-22 16:26:18 +0100

    Web server setup

--- apps/web/main.go
+++ apps/web/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"embed"
+	"fmt"
+	"net/http"
+)
+
+var (
+	//go:embed "templates/*"
+	templates embed.FS
+)
+
+func main() {
+	// todos := store.TodoStore{Items: []store.Todo{}}
+	// print.PrintTodos(os.Stdout, todos.Items...)
+
+	router := http.NewServeMux()
+	router.Handle("/", http.HandlerFunc(handler))
+
+	fmt.Println("Listening on port 5000...")
+	http.ListenAndServe(":5000", router)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Test")
+}
